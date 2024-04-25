f6b684582470a07b8614a88cbbe16abc09247e11
Frikandel <87879014+lenn219@users.noreply.github.com>
2024-04-25 16:53:50 +0200

    fix: serial print niet correct maar dit is voor opslag

--- src/main/main.ino
+++ src/main/main.ino
@@ -4,11 +4,11 @@ globale_logger logger;
 void setup() {
   // put your setup code here, to run once:
   logger = globale_logger();
+  delay(2000);
   logger.info("INFO");
   logger.warn("WARN");
   logger.error("ERROR");
   logger.fatal("CRASH");
-  delay(2000);
 
 }
 
