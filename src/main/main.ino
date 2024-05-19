34c892e9a82918d0e744edb80517fdd1b31de412
Frikandel <87879014+lenn219@users.noreply.github.com>
2024-05-19 21:11:59 +0200

    tussen tijdese commit

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,15 +1,14 @@
 #include <Wire.h>
 #include <Zumo32U4.h>
-#include "lijnSensor.h"
-#include "xbee.h"
 
-lijnSensor lijn;
-unsigned int lijnWaardes[3]= {0,0,0};
-xbee xbeeRadio;
+
 void setup() {
+  Serial1.println("test");
 }
 
 void loop() {
-
-
+  
+  Serial.println("test");
+  delay(500);
 }
+
